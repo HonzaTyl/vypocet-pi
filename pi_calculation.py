9010607387594edd2ac67c46bc2b591cf5816d75
Honza Tyl <40359242+HonzaTyl@users.noreply.github.com>
2025-05-18 17:03:14 +0200

    Update pi_calculation.py

--- pi_calculation.py
+++ pi_calculation.py
@@ -1,6 +1,6 @@
 # Výpočet π pomocí Leibnizovy řady
 def calculate_pi(n_terms=5):
-    pi = 0    pi = 0.0
+    pi = 0.0
     for k in range(n_terms):
         pi += ((-1) ** k) / (2 * k + 1)
     return 4 * pi
